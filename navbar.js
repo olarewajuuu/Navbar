8375db0ca070d65ae8964fc1026e2efd64f21321
Olarewaju Damilare <damilareolarewaju@yahoo.com>
2024-03-12 13:58:50 +0100

    my website navbar

---
+++ navbar.js
@@ -0,0 +1,8 @@
+let menuBar = document.getElementById('meun-bar')
+let navLink = document.getElementById('Nav-Link')
+
+
+function showmenu(){
+    menuBar.classList.toggle('fa-times')
+    navLink.classList.toggle('active')
+}
